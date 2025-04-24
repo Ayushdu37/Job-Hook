1bb44db3054575b313c02e725371e44cdfc80919
Ayush Dubey <ayushdu37@gmail.com>
2025-04-24 17:42:08 +0530

    Initial commit

--- src/Header/Header.tsx
+++ src/Header/Header.tsx
@@ -1,31 +1,35 @@
-import { Avatar } from "@mantine/core";
-import { IconAsset, IconBell, IconSettings } from "@tabler/icons-react";
+import { Avatar, Indicator } from "@mantine/core";
+import { IconAnchor, IconBell, IconSettings } from "@tabler/icons-react";
+import NavLinks from "./NavLinks";
 
 const Header = () => {
-    return (
-        <>
-            <div className="w-full bg-black text-white h-28 flex justify-between px-6 items-center">
-                <div className="flex gap-3 items-center">
-                    <IconAsset className="h-10 w-10" stroke={1.25}/>
-                    <div className="text-3xl font-semibold">iJobs</div>
-                </div>
-                <div className="flex gap-3">
-                    <a href="">Find Jobs</a>
-                    <a href="">Find Talent</a>
-                    <a href="">Upload Jobs</a>
-                    <a href="">About Us</a>
-                </div>
-                <div className="flex gap-5 items-center">
-                    <IconBell />
-                        <div className="flex items-center gap-2">
-                            <div>Ayush</div>
-                            <Avatar src="avatar.png" alt="it's me" />
-                        </div>
-                    <IconSettings />
-                </div>
-            </div>
-        </>
-    )
-}
+  return (
+    <>
+      <div className="w-full bg-mine-shaft-950 text-white h-20 flex justify-between px-6 items-center">
+        <div className="flex gap-1 items-center text-bright-sun-400">
+          <IconAnchor className="h-8 w-8" stroke={2.5} />
+          <div className="text-3xl font-semibold">JobHook</div>
+        </div>
+
+        {NavLinks()}
+
+        <div className="flex gap-3 items-center">
+          <div className="flex items-center gap-2">
+            <div>Ayush</div>
+            <Avatar src="avatar.png" alt="it's me" />
+          </div>
+          <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+            <IconSettings stroke={1.5} />
+          </div>
+          <div className="bg-mine-shaft-900 p-1.5 rounded-full">
+            <Indicator color="brightSun.4" offset={7} size={7} processing>
+              <IconBell stroke={1.5} />
+            </Indicator>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
 
 export default Header;
