d3cb463fef181e756a47d6360185926bf624de97
Ayush Dubey <ayushdu37@gmail.com>
2025-05-19 00:40:08 +0530

    PostJobPage 1/2

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
 
 import './App.css';
-import { createTheme, MantineProvider, Slider } from '@mantine/core';
+import { createTheme, Divider, MantineProvider, Slider } from '@mantine/core';
 import '@mantine/core/styles.css';
 import '@mantine/carousel/styles.css';
+import '@mantine/tiptap/styles.css';
 import HomePage from './pages/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import FindJobs from './pages/FIndJobs';
@@ -10,6 +11,7 @@ import Header from './Header/Header';
 import Footer from './Footer/Footer';
 import FindTalentPage from './pages/FIndTalentPage';
 import TalentProfilePage from './pages/TalentProfilePage';
+import PostJobPage from './pages/PostJobPage';
 
 function App() {
   const theme = createTheme({
@@ -17,15 +19,20 @@ function App() {
       'brightSun': ['#fffbeb','#fff3c6','#ffe588','#ffd149','#ffbd20','#f99b07','#dd7302','#b75006','#943c0c','#7a330d', '#461902',],
       'mineShaft': ['#f6f6f6','#e7e7e7','#d1d1d1','#b0b0b0','#888888','#6d6d6d','#5d5d5d','#4f4f4f','#454545','#3d3d3d', '#2d2d2d',],
     },
-    fontFamily:"poppins, sans-serif"
+    fontFamily:"poppins, sans-serif",
+    focusRing:"never",
+    primaryColor:'brightSun',
+    primaryShade:6,
   })
   return (
     <MantineProvider defaultColorScheme='dark' theme={theme}>
       <BrowserRouter>
       <Header/>
+      <Divider size="xs" />
         <Routes>
           <Route path='/find-jobs' element={<FindJobs/>}/>
           <Route path='/find-talent' element={<FindTalentPage/>}/>
+          <Route path='/post-job' element={<PostJobPage/>}/>
           <Route path='/talent-profile' element={<TalentProfilePage/>}/>
           <Route path='*' element={<HomePage/>}/>
         </Routes>
