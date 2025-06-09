bd3101aabb644a37aa185d04bc54b92effbd6d77
Ayush Dubey <ayushdu37@gmail.com>
2025-06-10 02:17:57 +0530

    Profile Page

--- src/Profile/CertiCard.tsx
+++ src/Profile/CertiCard.tsx
@@ -1,3 +1,6 @@
+import { ActionIcon } from "@mantine/core";
+import { IconPencil, IconTrash } from "@tabler/icons-react";
+
 const CertiCard = (props:any)=>{
     return <div className="flex justify-between">
             <div className="flex gap-2 items-center">
@@ -9,10 +12,14 @@ const CertiCard = (props:any)=>{
                     <div className="text-sm text-mine-shaft-300">{props.issuer}</div>
                 </div>
             </div>
+            <div className="flex items-center gap-2">
                 <div className="flex flex-col items-end">
                     <div text-sm text-mine-shaft-300>{props.issueDate}</div>
                     <div text-sm text-mine-shaft-300>{props.certificateId}</div>
                 </div>
+                {props.edit&&<ActionIcon size="lg" color="red.8" variant="subtle" >
+      <IconTrash className="h-4/5 w-4/5" stroke={1.5} /> </ActionIcon>}
+            </div>
         </div>
 }
 
