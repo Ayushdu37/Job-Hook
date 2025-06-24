4fac4c488371f8e3436248c94e75906c83dd8829
Ayush Dubey <ayushdu37@gmail.com>
2025-06-25 01:46:17 +0530

    Signup and Login API Integration

--- src/Components/SignUpLogin/SignUp.tsx
+++ src/Components/SignUpLogin/SignUp.tsx
@@ -2,18 +2,40 @@ import { Anchor, Button, Checkbox, Group, PasswordInput, Radio, TextInput } from
 import { IconAt, IconLock } from "@tabler/icons-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
+import { registerUser } from "../../Services/UserService";
+const form={
+  name:"",
+  email:"",
+  password:"",
+  confirmPassword:"",
+  accountType:"APPLICANT",
+}
+
 
 const SignUp = ()=>{
-    const [value, setValue] = useState('react');
+
+    const [data, setData] = useState(form); 
+
+    const handleChange = (event:any)=>{
+      if(typeof(event) == "string")setData({...data, accountType:event});
+      else setData({...data, [event.target.name]:event.target.value})
+    }
+
+    const handleSubmit = ()=>{
+      registerUser(data).then((res)=>{
+        console.log(res);
+      }).catch((err)=>console.log(err));
+    }
+
     return <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
         <div className="text-2xl font-semibold">Create Account</div>
-        <TextInput withAsterisk label="Full Name" placeholder="Your name"/>
-        <TextInput withAsterisk leftSection={<IconAt size={16} />} label="Email" placeholder="Your email"/>
-        <PasswordInput withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
-        <PasswordInput withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm password" />
+        <TextInput value={data.name} onChange={handleChange} name="name" withAsterisk label="Full Name" placeholder="Your name"/>
+        <TextInput value={data.email} onChange={handleChange} name="email" withAsterisk leftSection={<IconAt size={16} />} label="Email" placeholder="Your email"/>
+        <PasswordInput value={data.password} onChange={handleChange} name="password" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
+        <PasswordInput value={data.confirmPassword} onChange={handleChange} name="confirmPassword" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm password" />
         <Radio.Group
-      value={value}
-      onChange={setValue}
+      value={data.accountType}
+      onChange={handleChange}
       label="You are?"
       withAsterisk
     >
@@ -23,7 +45,7 @@ const SignUp = ()=>{
       </Group>
     </Radio.Group>
         <Checkbox autoContrast label={<>I accept{' '}<Anchor>terms & conditions</Anchor></>}/>
-        <Button autoContrast variant="filled">Sign up</Button>
+        <Button onClick={handleSubmit} autoContrast variant="filled">Sign up</Button>
         <div className="mx-auto">Have an account? <Link to="/login" className="text-bright-sun-400 hover:underline">Login</Link></div>
     </div>
 }
