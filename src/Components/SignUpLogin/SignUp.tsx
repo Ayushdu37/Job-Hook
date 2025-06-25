92ee2c0a11281a69cc03cebc2fee90cbf87e85e2
Ayush Dubey <ayushdu37@gmail.com>
2025-06-25 20:17:01 +0530

    Login and SignUp API Integration

--- src/Components/SignUpLogin/SignUp.tsx
+++ src/Components/SignUpLogin/SignUp.tsx
@@ -3,6 +3,7 @@ import { IconAt, IconLock } from "@tabler/icons-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 import { registerUser } from "../../Services/UserService";
+import { signupValidation } from "../../Services/FormValidation";
 const form={
   name:"",
   email:"",
@@ -14,25 +15,51 @@ const form={
 
 const SignUp = ()=>{
 
-    const [data, setData] = useState(form); 
+    const [data, setData] = useState<{[key:string]:string}>(form); 
+    const [formError, setFormError] = useState<{[key:string]:string}>(form);
 
     const handleChange = (event:any)=>{
-      if(typeof(event) == "string")setData({...data, accountType:event});
-      else setData({...data, [event.target.name]:event.target.value})
+      if(typeof(event) == "string"){
+        setData({...data, accountType:event});
+        return;
+      }
+      let name = event.target.name, value = event.target.value
+      setData({...data, [name]:value})
+      setFormError({...formError, [name]:signupValidation(name, value)})
+      if(name==="password" && data.confirmPassword !== ""){
+        let err="";
+        if(data.confirmPassword !== value)err="Password do not match.";
+        setFormError({...formError, [name]:signupValidation(name, value), confirmPassword:err});
+      }
+      if(name==="confirmPassword"){
+        if(data.password !== value)setFormError({...formError, [name]:"Password do not match."});
+          else setFormError({...formError, confirmPassword: ""});
+      }
     }
 
     const handleSubmit = ()=>{
-      registerUser(data).then((res)=>{
+      let valid = true, newFormError:{[key:string]:string}={};
+      for(let key in data){
+        if(key==="accoutType")continue;
+        if(key!=="confirmPassword")newFormError[key]=signupValidation(key,data[key]);
+        else if(data[key] !== data["password"])newFormError[key]="Password do not match."
+        if(newFormError[key])valid=false
+      }
+      setFormError(newFormError);
+      if(valid === true){
+        registerUser(data).then((res)=>{
         console.log(res);
+        
       }).catch((err)=>console.log(err));
+      }
     }
 
     return <div className="w-1/2 px-20 flex flex-col justify-center gap-3">
         <div className="text-2xl font-semibold">Create Account</div>
-        <TextInput value={data.name} onChange={handleChange} name="name" withAsterisk label="Full Name" placeholder="Your name"/>
-        <TextInput value={data.email} onChange={handleChange} name="email" withAsterisk leftSection={<IconAt size={16} />} label="Email" placeholder="Your email"/>
-        <PasswordInput value={data.password} onChange={handleChange} name="password" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
-        <PasswordInput value={data.confirmPassword} onChange={handleChange} name="confirmPassword" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm password" />
+        <TextInput value={data.name} error={formError.name} onChange={handleChange} name="name" withAsterisk label="Full Name" placeholder="Your name"/>
+        <TextInput value={data.email} error={formError.email} onChange={handleChange} name="email" withAsterisk leftSection={<IconAt size={16} />} label="Email" placeholder="Your email"/>
+        <PasswordInput value={data.password} error={formError.password} onChange={handleChange} name="password" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Password" placeholder="Password" />
+        <PasswordInput value={data.confirmPassword} error={formError.confirmPassword} onChange={handleChange} name="confirmPassword" withAsterisk leftSection={<IconLock size={18} stroke={1.5} />} label="Confirm Password" placeholder="Confirm password" />
         <Radio.Group
       value={data.accountType}
       onChange={handleChange}
